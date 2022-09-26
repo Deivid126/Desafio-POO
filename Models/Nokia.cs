79d6bf5a9d8dcc7bf2be04e1fb877cb9bd8be2a9
Deivid Gonçalves <deividg2022@outlook.com>
2022-09-26 20:35:05 -0300

    final commit

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,35 @@
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        
+
+         public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Intalando o aplicativo {nomeApp} no Nokia");
+        }
+
+
     }
+    
+
+    
 }
+
+        
+      
+        
+    
+ 
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero:"564616545",modelo:"Modelo 1",imei:"4545",memoria:45);
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero:"49849454645",modelo:"Modelo 2",imei:"7745",memoria:64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Poo");
+
+
